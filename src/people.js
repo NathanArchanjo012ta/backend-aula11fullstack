03d3db694c7e03ae1facc78aa5be517401a853a6
NathanArchanjo012ta <nathanaccouti21@gmail.com>
2025-10-21 17:07:19 -0300

    primeiro commit

---
+++ src/people.js
@@ -0,0 +1,22 @@
+
+
+const people = [
+    {
+        name: "Nathan Archanjo",
+        age: 10,
+        email: "nathanarchanjo@gmail.com"
+    },
+    {
+        name: "Breno Sena",
+        age: 17,
+        email: "brenosena@outlook.com"
+    },
+    {
+        name: "Raphael Brandão",
+        age: 12,
+        email: "raphaelbrandaodagigi@yahoo.com"
+    }
+    
+
+]
+export { people }
