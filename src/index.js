4ca16d9763aee7e66349ac6e37bd267519352b1d
NathanArchanjo012ta <nathanaccouti21@gmail.com>
2025-11-06 23:38:31 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,29 @@ import cors  from "cors"
 const app = express()
 const port = 3333
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (request, response)=>{
     response.json(people)
 
 })
+
+app.post("/cadastrar", (request, response) => {
+ const { name, email, age, password } = request.body.user;
+
+console.log(`
+Nome: ${name},
+E-mail: ${email},
+Idade: ${age},
+Senha: ${password}
+`);
+
+// guardar usuário no banco de dados
+
+response.status(201).json({ message: "Usuário cadastrado com sucesso!" });
+
+})
+
 
 app.listen(port, ()=>{
     console.log(`Servidor rodando na porta: ${port}`)
